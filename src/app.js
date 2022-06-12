89617d2b820e265cef4d23e3bf3e1f2c8e2fad2c
김아현 <dhflxhdxhd@gmail.com>
2022-06-13 01:48:19 +0900

    학습리스트 필터 기능 구현 (주차 수정해야함)

--- src/app.js
+++ src/app.js
@@ -13,14 +13,28 @@ function fetchClass(callback){
 })
 }
 
-function callbackFunc(data){
+function callbackFunc(btn,data){
+    const classData = data;
+    let checkBtn = btn;
+    console.log('classData',classData);
+
+    if (checkBtn === "all"){
+        showAll(classData);
+    } else if (checkBtn === "help"){
+        showHelp(classData);
+    } else if (checkBtn === "git"){
+        showGit(classData);
+    } else {
+        showRecent(classData);
+    }
+}
+
+function callbackFunc2(data){
     const classData = data
     console.log('classData',classData);
 
-    showAll(classData);
-    
+    showHelp(classData);  
 }
-
 
 function showAll(classData){
     let str = ""
@@ -46,4 +60,122 @@ function showAll(classData){
     table1.innerHTML = str
 }
 
-fetchClass((data) => callbackFunc(data))
+// 주차 수정
+function showHelp(classData){
+    let str = ""
+    let count = 1;
+
+    console.log("showJJe")
+    for (let i=0; i<classData.length; i++){
+        if (classData[i].links.length > 0){
+            str += `<tr><th scope="row">${count}</th>
+            <td>${classData[i].title}</td>
+            <td><a href="${classData[i].docUrl}" class="badge bg-secondary">문서</a></td>
+            <td>`
+            
+            for (let j=0; j<classData[i].links.length; j++){
+                str += `<a href="${classData[i].links[j]}" class="badge bg-secondary">${j+1}</a>`
+            }
+            
+            str += `</td>
+            <td>${classData[i].date}</td>
+            <td><a href=${classData[i].gitUrl}>git</a></td><tr>`
+    
+            count++;
+        }
+
+    }
+    table1.innerHTML = str
+}
+
+// 주차 수정
+function showGit(classData){
+    let str = ""
+    let count = 1;
+
+    console.log("showJJe")
+    for (let i=0; i<classData.length; i++){
+        if (classData[i].gitUrl && classData[i].gitUrl !== ""){
+            str += `<tr><th scope="row">${count}</th>
+            <td>${classData[i].title}</td>
+            <td><a href="${classData[i].docUrl}" class="badge bg-secondary">문서</a></td>
+            <td>`
+            
+            for (let j=0; j<classData[i].links.length; j++){
+                str += `<a href="${classData[i].links[j]}" class="badge bg-secondary">${j+1}</a>`
+            }
+            
+            str += `</td>
+            <td>${classData[i].date}</td>
+            <td><a href=${classData[i].gitUrl}>git</a></td><tr>`
+    
+            count++;
+        }
+
+    }
+    table1.innerHTML = str
+}
+
+// 날짜 최신순(내림차순) 정렬
+function date_recent(a,b){
+    const dateA = new Date(a['date']).getTime();
+    const dateB = new Date(b['date']).getTime();
+
+    return dateA < dateB ? 1 : -1;
+}
+// 주차 수정
+function showRecent(classData){
+    let str = ""
+    let count = 1;
+
+    console.log(classData.sort(date_recent));
+    showAll(classData)
+}
+
+function handleBtnAll(event){
+    btnAll.classList.add("active");
+    btnHelp.classList.remove("active");
+    btnGit.classList.remove("active");
+    btnRecent.classList.remove("active");
+
+    fetchClass((data) => callbackFunc("all",data))
+}
+
+function handleBtnHelp(){
+    btnHelp.classList.add("active");
+    btnAll.classList.remove("active");
+    btnGit.classList.remove("active");
+    btnRecent.classList.remove("active");
+
+    fetchClass((data) => callbackFunc("help",data))
+}
+
+function handleBtnGit(){
+    btnGit.classList.add("active");
+    btnAll.classList.remove("active");
+    btnHelp.classList.remove("active");
+    btnRecent.classList.remove("active");
+
+    fetchClass((data) => callbackFunc("git",data))
+}
+
+function handleBtnRecent(){
+    btnRecent.classList.add("active");
+    btnAll.classList.remove("active");
+    btnHelp.classList.remove("active");
+    btnGit.classList.remove("active");
+
+    fetchClass((data) => callbackFunc("recent",data))
+}
+
+function init(){
+    fetchClass((data) => callbackFunc("all",data))
+
+    btnAll.addEventListener("click",handleBtnAll);
+    btnHelp.addEventListener("click",handleBtnHelp);
+    btnGit.addEventListener("click",handleBtnGit);
+    btnRecent.addEventListener("click",handleBtnRecent);
+}
+
+
+init();
